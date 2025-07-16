1d483ce452e64a6ec85b4fcdffc29dd679763c0e
GabSoto <gsotocco@unsa.edu.pe>
2025-07-15 23:12:51 -0500

    Changes por pull request.

--- auth-service/src/main/java/com/sistema/auth/controller/UsuarioController.java
+++ auth-service/src/main/java/com/sistema/auth/controller/UsuarioController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,8 +21,6 @@ import com.sistema.auth.service.UsuarioService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/usuario")
@@ -34,9 +31,6 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
-
-    @Autowired
-    private ModelMapper modelMapper;
 
     @PostMapping("/register")
     public ResponseEntity<?> registrarUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO) {
